31ae7601e1c630c17ada480801880c16d55fe52d
Dax Harris <matteovh@gmail.com>
2024-06-12 17:16:08 -0400

    extract script testing

--- rominator/src-tauri/src/main.rs
+++ rominator/src-tauri/src/main.rs
@@ -1,10 +1,78 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::path::{Path, PathBuf};
+use async_zip::base::read::seek::ZipFileReader;
+use tokio::{
+    fs::{create_dir_all, File, OpenOptions},
+    io::BufReader,
+};
+use tokio_util::compat::{TokioAsyncReadCompatExt, TokioAsyncWriteCompatExt};
+
+/// Returns a relative path without reserved names, redundant separators, ".", or "..".
+fn sanitize_file_path(path: &str) -> PathBuf {
+    // Replaces backwards slashes
+    path.replace('\\', "/")
+        // Sanitizes each component
+        .split('/')
+        .map(sanitize_filename::sanitize)
+        .collect()
+}
+
+/// Extracts everything from the ZIP archive to the output directory
+async fn unzip_file(archive: File, out_dir: &Path) {
+    let archive = BufReader::new(archive).compat();
+    let mut reader = ZipFileReader::new(archive).await.expect("Failed to read zip file");
+    for index in 0..reader.file().entries().len() {
+        let entry = reader.file().entries().get(index).unwrap();
+        let path = out_dir.join(sanitize_file_path(entry.filename().as_str().unwrap()));
+        // If the filename of the entry ends with '/', it is treated as a directory.
+        // This is implemented by previous versions of this crate and the Python Standard Library.
+        // https://docs.rs/async_zip/0.0.8/src/async_zip/read/mod.rs.html#63-65
+        // https://github.com/python/cpython/blob/820ef62833bd2d84a141adedd9a05998595d6b6d/Lib/zipfile.py#L528
+        let entry_is_dir = entry.dir().unwrap();
+
+        let mut entry_reader = reader.reader_without_entry(index).await.expect("Failed to read ZipEntry");
+
+        if entry_is_dir {
+            // The directory may have been created if iteration is out of order.
+            if !path.exists() {
+                create_dir_all(&path).await.expect("Failed to create extracted directory");
+            }
+        } else {
+            // Creates parent directories. They may not exist if iteration is out of order
+            // or the archive does not contain directory entries.
+            let parent = path.parent().expect("A file entry should have parent directories");
+            if !parent.is_dir() {
+                create_dir_all(parent).await.expect("Failed to create parent directories");
+            }
+            let writer = OpenOptions::new()
+                .write(true)
+                .create_new(true)
+                .open(&path)
+                .await
+                .expect("Failed to create extracted file");
+            futures_lite::io::copy(&mut entry_reader, &mut writer.compat_write())
+                .await
+                .expect("Failed to copy to extracted file");
+
+            // Closes the file and manipulates its metadata here if you wish to preserve its metadata from the archive.
+        }
+    }
+}
+
+#[tauri::command]
+async fn extract_file(archive_path: String, extract_path: String) {
+  let archive = File::open(archive_path).await.expect("Failed to open archive");
+  let out_dir: &Path = Path::new(&extract_path);
+  unzip_file(archive, out_dir).await;
+}
+
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_persisted_scope::init())
         .plugin(tauri_plugin_upload::init())
+        .invoke_handler(tauri::generate_handler![extract_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- rominator/src/view/root.tsx
+++ rominator/src/view/root.tsx
@@ -2,6 +2,7 @@ import {
     ActionIcon,
     AppShell,
     Box,
+    Button,
     Divider,
     Group,
     Paper,
@@ -15,6 +16,7 @@ import {
     IconSearch,
     IconSettings,
 } from "@tabler/icons-react";
+import { invoke } from "@tauri-apps/api/tauri";
 
 export function RootView() {
     return (
@@ -94,7 +96,19 @@ export function RootView() {
                                 </Group>
                             </Tabs.Tab>
                         </Tabs.List>
-                        <Tabs.Panel value="search">EEE</Tabs.Panel>
+                        <Tabs.Panel value="search">
+                            <Button
+                                onClick={() =>
+                                    invoke("extract_file", {
+                                        archivePath:
+                                            "/home/itec/Downloads/retroarch-assets-1.19.0.zip",
+                                        extractPath: "/home/itec/Downloads",
+                                    }).then(console.log)
+                                }
+                            >
+                                PRESS ME
+                            </Button>
+                        </Tabs.Panel>
                         <Tabs.Panel value="library">EEE</Tabs.Panel>
                     </Tabs>
                 </Box>
