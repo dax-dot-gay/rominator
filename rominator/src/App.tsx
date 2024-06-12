ca93fa75130c6bb53bbf9e2041eef0badfa9a111
Dax Harris <matteovh@gmail.com>
2024-06-12 15:02:13 -0400

    UI scaffold

--- rominator/src/App.tsx
+++ rominator/src/App.tsx
@@ -1,7 +1,38 @@
+import { MantineProvider, createTheme } from "@mantine/core";
 import "./style/index.scss";
+import { ModalsProvider } from "@mantine/modals";
+import { Notifications } from "@mantine/notifications";
+import { RootView } from "./view/root";
 
 function App() {
-    return <></>;
+    return (
+        <MantineProvider
+            defaultColorScheme="dark"
+            theme={createTheme({
+                colors: {
+                    primary: [
+                        "#faedff",
+                        "#edd9f7",
+                        "#d8b1ea",
+                        "#c286dd",
+                        "#ae62d2",
+                        "#a24bcb",
+                        "#9e3fc9",
+                        "#8931b2",
+                        "#7b2aa0",
+                        "#6b218d",
+                    ],
+                },
+                primaryColor: "primary",
+                primaryShade: 7,
+            })}
+        >
+            <ModalsProvider>
+                <Notifications />
+                <RootView />
+            </ModalsProvider>
+        </MantineProvider>
+    );
 }
 
 export default App;
---
+++ rominator/src/view/root.tsx
@@ -0,0 +1,104 @@
+import {
+    ActionIcon,
+    AppShell,
+    Box,
+    Divider,
+    Group,
+    Paper,
+    Tabs,
+    Text,
+} from "@mantine/core";
+import {
+    IconDeviceGamepad2,
+    IconDownload,
+    IconLibrary,
+    IconSearch,
+    IconSettings,
+} from "@tabler/icons-react";
+
+export function RootView() {
+    return (
+        <AppShell header={{ height: 64 }} className="app-root" p="sm">
+            <AppShell.Header
+                className="app-root-section header"
+                withBorder={false}
+                p="sm"
+            >
+                <Group justify="space-between" wrap="nowrap" gap="sm">
+                    <Paper
+                        p="xs"
+                        className="app-name header-section"
+                        shadow="xs"
+                    >
+                        <Group gap="sm" justify="center">
+                            <IconDeviceGamepad2 />
+                            <Divider orientation="vertical" />
+                            <Text
+                                size="lg"
+                                ff="monospace"
+                                style={{ transform: "translate(0, 1px)" }}
+                            >
+                                Rominator
+                            </Text>
+                        </Group>
+                    </Paper>
+                    <Paper
+                        className="app-actions header-section"
+                        p="0"
+                        shadow="xs"
+                    >
+                        <Group gap="0">
+                            <ActionIcon
+                                className="app-action"
+                                variant="transparent"
+                            >
+                                <IconDownload />
+                            </ActionIcon>
+                            <ActionIcon
+                                className="app-action"
+                                variant="transparent"
+                            >
+                                <IconSettings />
+                            </ActionIcon>
+                        </Group>
+                    </Paper>
+                </Group>
+            </AppShell.Header>
+            <AppShell.Main className="app-wrapper">
+                <Box className="app-content">
+                    <Tabs
+                        className="main-nav"
+                        orientation="vertical"
+                        defaultValue="search"
+                        classNames={{ panel: "nav-panel" }}
+                    >
+                        <Tabs.List>
+                            <Tabs.Tab value="search">
+                                <Group
+                                    gap="sm"
+                                    justify="space-between"
+                                    align="start"
+                                >
+                                    <IconSearch size={20} />
+                                    <Text>Search</Text>
+                                </Group>
+                            </Tabs.Tab>
+                            <Tabs.Tab value="library">
+                                <Group
+                                    gap="sm"
+                                    justify="space-between"
+                                    align="start"
+                                >
+                                    <IconLibrary size={20} />
+                                    <Text>Library</Text>
+                                </Group>
+                            </Tabs.Tab>
+                        </Tabs.List>
+                        <Tabs.Panel value="search">EEE</Tabs.Panel>
+                        <Tabs.Panel value="library">EEE</Tabs.Panel>
+                    </Tabs>
+                </Box>
+            </AppShell.Main>
+        </AppShell>
+    );
+}
